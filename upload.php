31edb9efaf6747a278e9739098c5c2c3605c5124
mossfelt <noahfelt02@gmail.com>
2023-07-20 12:28:13 -0600

    upload.php

--- upload.php
+++ upload.php
@@ -1,7 +1,22 @@
 <?php
 print_r($_FILES);
-$filename = $_FILES['filetoupload']["name"];
 if (isset($filename)) {
     echo $filename;
 }
+$targetdirectory = "uploads/";
+$filename = $targetdirectory . basename($_FILES['filetoupload']['name']);
+$file_type = pathinfo($filename, PATHINFO_EXTENSION);
+if (!empty($FILES['filetoupload']['name'])) {
+    $allowtypes = array('jpg', 'png', 'jpeg', 'gif');
+    if (in_array($file_type, $allowtypes)) {
+        if (move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)) {
+            echo 'successfully uploaded file!';
+        } else {
+            echo 'failed to upload file :(';
+        }
+    } else {
+        echo 'wrong type of file, dingus';
+    }
+}
+
 ?>
