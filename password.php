1f4b7a736e2b63f3fa0bb1aca06326a68551addf
mossfelt <139724715+mossfelt@users.noreply.github.com>
2023-07-19 22:10:24 -0600

    fixed bug

--- password.php
+++ password.php
@@ -7,7 +7,7 @@ $password = $_POST['password'];
 $db = new mysqli($servername, $db_username, $db_password);
 $query = "SELECT password from users where username='$username'";
 $result = $db->query($query);
-$row = $result -> fetch_assoc(mysqli_num);
+$row = $result -> fetch_assoc();
 if(password_verify($password,$row['password'])){
     echo 'congrats!';
 }
