a263e389378637fc0e8cb1acf0affa1e1d5d1172
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-20 16:34:00 -0600

    cleanup

--- password.php
+++ password.php
@@ -5,13 +5,12 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $query = "SELECT password from users where username='$username'";
 $result = $db->query($query);
-$row = $result -> fetch_assoc();
-if(isset($row) && password_verify($password,$row['password'])){
+$row = $result->fetch_assoc();
+if (isset($row) && password_verify($password, $row['password'])) {
     session_regenerate_id();
     $_SESSION['loggedin'] = true;
     header('Location: secretgallery.php');
-}
-else{
+} else {
     echo 'too bad :(';
 }
 ?>
--- upload.php
+++ upload.php
@@ -1,19 +1,14 @@
 <?php
 include '/home/mabufudy/db.php';
 $targetdirectory = "uploads/";
-// $filename = $targetdirectory . basename($_FILES['filetoupload']['name']);
 $path_parts = pathinfo($_FILES['filetoupload']['name']);
 $file_type = $path_parts['extension'];
 $filename = $targetdirectory . $path_parts['filename'] . '_' . time() . '.' . $path_parts['extension'];
 if (!empty($_POST['tag'])) {
     $tag = $_POST['tag'];
-    // $file_type = pathinfo($filename, PATHINFO_EXTENSION);
     if (!empty($_FILES['filetoupload']['name'])) {
         $allowtypes = array('jpg', 'png', 'jpeg', 'gif');
         if (in_array($file_type, $allowtypes)) {
-            // $path_parts = pathinfo($_FILES['filetoupload']['name']);
-            // $timestamped_ = $path_parts['filename'] . '_' . time() . '.' . $path_parts['extension'];
-            // if (move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)) {
             if (move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)) {
                 $insert = $db->query("INSERT into images (filepath,tag,date) VALUES ('$filename', '$tag', NOW())");
                 if ($insert) {
@@ -33,5 +28,4 @@ if (!empty($_POST['tag'])) {
 } else {
     echo 'you must choose a tag';
 }
-
 ?>
