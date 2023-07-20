455689a2e013162b0a5761b120510fd286cb6799
mossfelt <139724715+mossfelt@users.noreply.github.com>
2023-07-19 22:13:33 -0600

    hopefully fixed the bug

--- password.php
+++ password.php
@@ -2,9 +2,10 @@
 $servername = 'mabufudyne.com';
 $db_username = 'mabufudy_primary';
 $db_password = 'U7fBt*hUoytp@tcNu4TUgs!Hw';
+$db_name = 'mabufudy_db';
 $username = $_POST['username'];
 $password = $_POST['password'];
-$db = new mysqli($servername, $db_username, $db_password);
+$db = new mysqli($servername, $db_username, $db_password, $db_name);
 $query = "SELECT password from users where username='$username'";
 $result = $db->query($query);
 $row = $result -> mysqli_fetch_assoc();
