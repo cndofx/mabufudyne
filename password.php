acdf6a80fa90be6b1d3547a07c641c8930956d0f
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-19 22:38:05 -0600

    Update password.php

--- password.php
+++ password.php
@@ -1,11 +1,12 @@
 <?php
-$servername = 'mabufudyne.com';
-$db_username = 'mabufudy_primary';
-$db_password = 'U7fBt*hUoytp@tcNu4TUgs!Hw';
-$db_name = 'mabufudy_db';
+// $servername = 'mabufudyne.com';
+// $db_username = 'mabufudy_primary';
+// $db_password = 'U7fBt*hUoytp@tcNu4TUgs!Hw';
+// $db_name = 'mabufudy_db';
+include(dirname(dirname(__DIR__)).'db.php');
 $username = $_POST['username'];
 $password = $_POST['password'];
-$db = new mysqli($servername, $db_username, $db_password, $db_name);
+// $db = new mysqli($servername, $db_username, $db_password, $db_name);
 $query = "SELECT password from users where username='$username'";
 $result = $db->query($query);
 $row = $result -> fetch_assoc();
