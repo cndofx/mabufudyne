ec018b2aded432bfc389be274898092e1d4c4a3c
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-20 15:57:22 -0600

    require tag to be set when uploading

--- upload.php
+++ upload.php
@@ -4,25 +4,28 @@ $targetdirectory = "uploads/";
 $filename = $targetdirectory . basename($_FILES['filetoupload']['name']);
 if (isset($_POST['tag'])) {
     $tag = $_POST['tag'];
-}
-$file_type = pathinfo($filename, PATHINFO_EXTENSION);
-if (!empty($_FILES['filetoupload']['name'])) {
-    $allowtypes = array('jpg', 'png', 'jpeg', 'gif');
-    if (in_array($file_type, $allowtypes)) {
-        if (move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)) {
-            $insert = $db->query("INSERT into images (filepath,tag,date) VALUES ('$filename', '$tag', NOW())");
-            if ($insert) {
-                header('Location: secretgallery.php');
+    $file_type = pathinfo($filename, PATHINFO_EXTENSION);
+    if (!empty($_FILES['filetoupload']['name'])) {
+        $allowtypes = array('jpg', 'png', 'jpeg', 'gif');
+        if (in_array($file_type, $allowtypes)) {
+            if (move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)) {
+                $insert = $db->query("INSERT into images (filepath,tag,date) VALUES ('$filename', '$tag', NOW())");
+                if ($insert) {
+                    header('Location: secretgallery.php');
+                } else {
+                    echo 'the file has not gone to the database';
+                }
             } else {
-                echo 'the file has not gone to the database';
+                echo 'failed to upload file :(';
             }
         } else {
-            echo 'failed to upload file :(';
+            echo 'wrong type of file, dingus';
         }
     } else {
-        echo 'wrong type of file, dingus';
+        echo 'you have to upload a file';
     }
 } else {
-    echo 'you have to upload a file';
+    echo 'you must choose a tag';
 }
+
 ?>
