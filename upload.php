b2804862f7cff7bf94f4979a903c020a123408dc
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-20 16:25:41 -0600

    timestamp uploads

--- upload.php
+++ upload.php
@@ -1,13 +1,19 @@
 <?php
 include '/home/mabufudy/db.php';
 $targetdirectory = "uploads/";
-$filename = $targetdirectory . basename($_FILES['filetoupload']['name']);
+// $filename = $targetdirectory . basename($_FILES['filetoupload']['name']);
+$path_parts = pathinfo($_FILES['filetoupload']['name']);
+$file_type = $path_parts['extension'];
+$filename = $targetdirectory . $path_parts['filename'] . '_' . time() . '.' . $path_parts['extension'];
 if (!empty($_POST['tag'])) {
     $tag = $_POST['tag'];
-    $file_type = pathinfo($filename, PATHINFO_EXTENSION);
+    // $file_type = pathinfo($filename, PATHINFO_EXTENSION);
     if (!empty($_FILES['filetoupload']['name'])) {
         $allowtypes = array('jpg', 'png', 'jpeg', 'gif');
         if (in_array($file_type, $allowtypes)) {
+            // $path_parts = pathinfo($_FILES['filetoupload']['name']);
+            // $timestamped_ = $path_parts['filename'] . '_' . time() . '.' . $path_parts['extension'];
+            // if (move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)) {
             if (move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)) {
                 $insert = $db->query("INSERT into images (filepath,tag,date) VALUES ('$filename', '$tag', NOW())");
                 if ($insert) {
