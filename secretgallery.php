7c8053c0ffac09f51b978351975d5b84a10c9b9c
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-20 13:37:12 -0600

    add wrapper tag

--- secretgallery.php
+++ secretgallery.php
@@ -14,40 +14,42 @@ if (!isset($_SESSION['loggedin'])) {
 
     <body>
         <h1>secret art gallery</h1>
-        <div class="file_upload">
-            <form method="post" action="upload.php" enctype="multipart/form-data">
-                <input type="file" name="filetoupload">
-                <input type="submit" name="submit">
-                <select name="tag" for="tags" id="tags">
-                    <option value="">select</option>
-                    <option value="not explicit">not explicit</option>
-                    <option value="explicit">explicit</option>
-                </select>
-            </form>
-        </div>
-        <div class="text-box">
-            <p>welcome to the art gallery, enjoy your stay</p>
-        </div>
-        <h1 class="explicit">explicit</h1>
-        <?php
-        $query = "select * from images where tag='explicit'";
-        $result = $db->query($query);
-        while ($data = mysqli_fetch_assoc($result)) {
-            ?>
-            <img src="/<?php echo $data['filepath']; ?>">
+        <div class="wrapper">
+            <div class="file_upload">
+                <form method="post" action="upload.php" enctype="multipart/form-data">
+                    <input type="file" name="filetoupload">
+                    <input type="submit" name="submit">
+                    <select name="tag" for="tags" id="tags">
+                        <option value="">select</option>
+                        <option value="not explicit">not explicit</option>
+                        <option value="explicit">explicit</option>
+                    </select>
+                </form>
+            </div>
+            <div class="text-box">
+                <p>welcome to the art gallery, enjoy your stay</p>
+            </div>
+            <h1 class="explicit">explicit</h1>
             <?php
-        }
-        ?>
-        <h1>not explicit</h1>
-        <?php
-        $query = "select * from images where tag='not explicit'";
-        $result = $db->query($query);
-        while ($data = mysqli_fetch_assoc($result)) {
+            $query = "select * from images where tag='explicit'";
+            $result = $db->query($query);
+            while ($data = mysqli_fetch_assoc($result)) {
+                ?>
+                <img src="/<?php echo $data['filepath']; ?>">
+                <?php
+            }
             ?>
-            <img src="/<?php echo $data['filepath']; ?>">
+            <h1>not explicit</h1>
             <?php
-        }
-        ?>
+            $query = "select * from images where tag='not explicit'";
+            $result = $db->query($query);
+            while ($data = mysqli_fetch_assoc($result)) {
+                ?>
+                <img src="/<?php echo $data['filepath']; ?>">
+                <?php
+            }
+            ?>
+        </div>
     </body>
 
     </html>
