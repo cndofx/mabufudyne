02885cd3fa962a9b61846f9dfcb3fc117851837a
mossfelt <139724715+mossfelt@users.noreply.github.com>
2023-07-19 21:58:22 -0600

    password system

--- password.php
+++ password.php
@@ -1,3 +1,18 @@
 <?php
 echo $_POST['username'];
+$servername = 'mabufudyne.com';
+$db_username = 'mabufudy_primary';
+$db_password = 'U7fBt*hUoytp@tcNu4TUgs!Hw'
+$username = $_POST['username'];
+$password = $_POST['password'];
+$db = new mysqli($servername, $db_username, $db_password);
+$query = "SELECT password from users where username='$username'";
+$result = $db->query($query);
+$row = $result -> fetch_assoc(mysqli_num);
+if(password_verify($password,$row['password'])){
+    echo 'congrats!';
+}
+else{
+    echo 'too bad :(';
+}
 ?>
