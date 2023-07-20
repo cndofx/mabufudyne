5a01696c6774565b3614b7b025916b6294e99537
mossfelt <noahfelt02@gmail.com>
2023-07-20 12:50:49 -0600

    added redirect

--- upload.php
+++ upload.php
@@ -1,8 +1,5 @@
 <?php
 include '/home/mabufudy/db.php';
-if (isset($filename)) {
-    echo $filename;
-}
 $targetdirectory = "uploads/";
 $filename = $targetdirectory . basename($_FILES['filetoupload']['name']);
 $file_type = pathinfo($filename, PATHINFO_EXTENSION);
@@ -10,10 +7,9 @@ if (!empty($_FILES['filetoupload']['name'])) {
     $allowtypes = array('jpg', 'png', 'jpeg', 'gif');
     if (in_array($file_type, $allowtypes)) {
         if (move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)) {
-            echo 'successfully uploaded file!';
             $insert = $db->query("INSERT into images (filepath,date) VALUES ('$filename', NOW())");
             if ($insert) {
-                echo 'the file has gone to the database';
+                header('Location secretgallery.php');
             } else {
                 echo 'the file has not gone to the database';
             }
