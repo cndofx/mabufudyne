446bb774e1ef34d9a9cb9bc19d3a804403019ead
mossfelt <noahfelt02@gmail.com>
2023-07-22 08:26:38 -0600

    debug text

--- searchtags.php
+++ searchtags.php
@@ -1,6 +1,7 @@
 <?php
 include '/home/mabufudy/db.php';
 $searchtags=$_POST['searchtags'];
+print_r($searchtags);
 $sql="select images.filepath from tags inner join images on images.imageid = tags.imageid where tags.tag='$searchtags'";
 $result=$db->query($sql); 
 if (isset($result)){
