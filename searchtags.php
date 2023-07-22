c3fc4d415668e754700b7ea84060a6a710788101
mossfelt <noahfelt02@gmail.com>
2023-07-22 13:11:55 -0600

    added more css to search page

--- searchtags.php
+++ searchtags.php
@@ -1,19 +1,24 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="gallery.css">
+    </head>
+    <body>
+<h1>search results</h1>
 <?php
 include '/home/mabufudy/db.php';
 $searchtags=$_POST['searchtags'];
 print_r($searchtags);
 $sql="select images.filepath from tags inner join images on images.imageid = tags.imageid where tags.tag='$searchtags'";
 $result=$db->query($sql); 
+?>
+<div class="image-columns">
+<?php
 while ($row=mysqli_fetch_assoc($result)){
         ?>
         <img src="/<?php echo $row['filepath'];?>">
         <?php
     }   
 ?>
-<html>
-    <head>
-    <link rel="stylesheet" href="gallery.css">
-    </head>
-    <body>
-    </body>
+</div>
+</body>
 </html>
