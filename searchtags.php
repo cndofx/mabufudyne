fe5e5009372e680073351625f77a62af6c55a4df
mossfelt <noahfelt02@gmail.com>
2023-07-22 08:21:27 -0600

    added images to the search page

--- searchtags.php
+++ searchtags.php
@@ -4,7 +4,10 @@ $searchtags=$_POST['searchtags'];
 $sql="select images.filepath from tags inner join images on images.imageid = tags.imageid where tags.tag='$searchtags'";
 $result=$db->query($sql); 
 if (isset($result)){
-    $row=mysqli_fetch_assoc($result);
-    print_r($row);
+    while ($row=mysqli_fetch_assoc($result)){
+        ?>
+        <img src="/<?php echo $row['filepath'];?>"
+        <?php
+    }   
 }
 ?>
