21c6c484ffaf83c3e9a0c52da8e91d7e0cc1da0b
mossfelt <139724715+mossfelt@users.noreply.github.com>
2023-07-23 17:11:20 -0600

    delete image script

---
+++ deleteimage.php
@@ -0,0 +1,16 @@
+<?php
+include '/home/mabufudy/db.php';
+$imageid = $_GET['imageid'];
+$query="DELETE images 
+		FROM images 
+		LEFT JOIN tags 
+		ON images.imageid = tags.imageid
+		WHERE images.imageid='$imageid'"
+$delete=$db->query($query);
+if($delete){
+	echo 'image deleted';
+}
+else{
+	echo 'image not deleted';
+}
+?>
