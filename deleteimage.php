b3a4c95897733549010d2bc5fd625f693af8580b
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-23 17:29:18 -0600

    verify user auth before deleting

--- deleteimage.php
+++ deleteimage.php
@@ -1,15 +1,23 @@
 <?php
+session_start();
 include '/home/mabufudy/db.php';
-$imageid = $_GET['imageid'];
-$query = "DELETE images, tags
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == FALSE) {
+	header('Location: gallery.html');
+}
+?>
+<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE) {
+	include '/home/mabufudy/db.php';
+	$imageid = $_GET['imageid'];
+	$query = "DELETE images, tags
 		FROM images 
 		LEFT JOIN tags 
 		ON images.imageid = tags.imageid
 		WHERE images.imageid='$imageid'";
-$delete = $db->query($query);
-if ($delete) {
-	echo 'image deleted';
-} else {
-	echo 'image not deleted';
+	$delete = $db->query($query);
+	if ($delete) {
+		echo 'image deleted';
+	} else {
+		echo 'image not deleted';
+	}
 }
 ?>
