1f44fc9814c165af3c7b5b5f8e80dc8b60a67315
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-23 17:32:13 -0600

    get filepath of image to delete

--- deleteimage.php
+++ deleteimage.php
@@ -8,16 +8,22 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == FALSE) {
 <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE) {
 	include '/home/mabufudy/db.php';
 	$imageid = $_GET['imageid'];
-	$query = "DELETE images, tags
-		FROM images 
-		LEFT JOIN tags 
-		ON images.imageid = tags.imageid
-		WHERE images.imageid='$imageid'";
-	$delete = $db->query($query);
-	if ($delete) {
-		echo 'image deleted';
-	} else {
-		echo 'image not deleted';
+	$query = "SELECT filepath FROM images WHERE imageid='$imageid'";
+	$row = $db->query($query);
+	if (isset($row['filepath'])) {
+		$filepath = $row['filepath'];
+		echo 'image path: ' . $filepath;
 	}
+	// $query = "DELETE images, tags
+	// 	FROM images 
+	// 	LEFT JOIN tags 
+	// 	ON images.imageid = tags.imageid
+	// 	WHERE images.imageid='$imageid'";
+	// $delete = $db->query($query);
+	// if ($delete) {
+	// 	echo 'image deleted';
+	// } else {
+	// 	echo 'image not deleted';
+	// }
 }
 ?>
